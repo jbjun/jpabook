50828dd0089c1ad70c3ae9ab50723f596f119406
qudwns4351 <qudwns4351@naver.com>
2023-12-13 00:10:33 +0900

    Initial commit

---
+++ src/test/java/jpabook/jpabook/JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
